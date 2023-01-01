382dc8aa44f3babfe9b1684f69c4a07fd2b44291
Hailey Loechler <haileyloechler@gmail.com>
2023-01-01 14:52:35 -0800

    added sequelize connection and models created

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,13 +5,21 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category,{
+  foreignKey: 'category_id',
+})
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_name'
+})
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsTo(Tag,{
+  foreignKey: 'tag_id',
+})
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsTo(Product,{
+  foreignKey: 'product_id',
+})
 module.exports = {
   Product,
   Category,
